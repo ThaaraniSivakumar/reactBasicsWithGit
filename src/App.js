5cd612568e6b4e432d1bbc2ecec2f66a969562c6
ThaaraniShivakumar <sivathaarani2004@gmail.com>
2025-06-03 21:11:05 +0530

    created input field and tried to perform to do list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Counter } from "./Practice/Counter";
 import { DigitalClock } from "./Practice/DigitalClock";
+import { ToDoList } from "./Practice/ToDoList.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <DigitalClock/>
+      {/* <Counter/>
+      <DigitalClock/> */}
+      <ToDoList/>
     </div>
   );
 }
--- src/Practice/ToDoList.jsx.jsx
+++ src/Practice/ToDoList.jsx.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+
 export const ToDoList=()=>{
+    const[list,setList]=useState([]);
+    const [addList,setAddList]=useState()
+    const getInputFiled=()=>{
+        setList()
+    }
     return(
-        <div></div>
+        <div>
+            <input placeholder="Enter you thought" onChange={(e)=>setList(e.target.value)}></input>
+            <button onClick={getInputFiled}>Add</button>
+            <div>
+                <div>{list}</div>
+                <button >Delete</button>
+            </div>
+        </div>
     )
 }
